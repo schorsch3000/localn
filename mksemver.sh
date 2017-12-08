46de3b4280c07e306a1c6a2233a29b93df6d4944
Dirk Heilig <heilig@adesso-mobile.de>
2017-12-08 10:38:57 +0100

    fix node version vorr semver bild

--- mksemver.sh
+++ mksemver.sh
@@ -3,7 +3,7 @@ rm -rf semverbuild
 mkdir semverbuild
 cd semverbuild
 $(../src/localn.sh init)
-../src/localn.sh stable
+../src/localn.sh 8.0.0
 ../src/localn.sh module semver
 ../src/localn.sh module pkg
 pkg .localn/node-*/lib/node_modules/semver/
